783601e4d2a1d192d57d81cb1c81ea9280a029c3
John Napiorkowski <jjn1056@yahoo.com>
2011-05-09 21:55:28 -0400

    initial commit

---
+++ lib/Catalyst/ActionRole/MatchRequestAccepts.pm
@@ -0,0 +1,38 @@
+package Catalyst::ActionRole::MatchRequestAccepts;
+
+use 5.008008;
+use strict;
+use warnings FATAL =>'all';
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+Catalyst::ActionRole::MatchRequestAccepts - My Brand New Module
+
+=head1 SYNOPSIS
+
+  use Catalyst::ActionRole::MatchRequestAccepts;
+
+=head1 DESCRIPTION
+
+Catalyst::ActionRole::MatchRequestAccepts is ...
+
+=head1 AUTHOR
+
+John Napiorkowski L<email:jjnapiork@cpan.org>
+
+=head1 SEE ALSO
+
+    TBD
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011, John Napiorkowski L<email:jjnapiork@cpan.org> 
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
